e9d8e4aef4963b7f5c7341e8dd513048d0d281e0
Thiago Sagara <thiagosagara@gmail.com>
2022-02-22 12:54:49 -0300

    [EC2] - Atualizacao logica
    Alterado para coletar a Tag Name
    Adicionado o parse
    Iniciado o processo de criacao de funcoes
    Alterado a forma de conexao em sessao

--- aws-py-ec2-describe.py
+++ aws-py-ec2-describe.py
@@ -4,31 +4,81 @@
 # precise que voce passe o nome do profile da conta
 
 import boto3
-from botocore.config import Config
 import os
+import argparse
+from pprint import pprint
 
 
-profile = str(input("Digite o profile: "))
-os.system('cls' if os.name == 'nt' else 'clear')
 
-session = boto3.session.Session(profile_name = profile)
-client = session.client('ec2')
-
-regions = client.describe_regions()
-print("account","region","instanceid","instancetype","hostname",sep=",")
 
 def list_region(region, get_profile):
- mc=Config( region_name = region )
- conn=boto3.client('ec2', config=mc)
+ session = boto3.session.Session(profile_name = str(gprofile), region_name = region)
+ conn=session.client('ec2')
  
  response = conn.describe_instances()
  
  for r in response['Reservations']:
   for i in r['Instances']:
-   if "KeyName" in i:
-    print(f"{profile},{region},{i['InstanceId']},{i['InstanceType']},{i['KeyName']}")
+   if "Tags" in i:
+    for t in i['Tags']:
+     if t['Key'] == 'Name':
+      print(f"{get_profile},{region},{i['InstanceId']},{i['InstanceType']},{t['Value']},{i['State']['Name']}")
+   
    else:
-    print(f"{profile},{region},{i['InstanceId']},{i['InstanceType']},Sem Hostname")
+    print(f"{get_profile},{region},{i['InstanceId']},{i['InstanceType']},Sem Hostname,{i['State']['Name']}")
+ 
+ return
 
-for i in regions['Regions']:
- list_region(i['RegionName'], profile)
+
+#==============================================================================
+# ---- Inicio do programa
+#==============================================================================
+
+if __name__ == '__main__':
+
+ #Limpa a tela
+ os.system('cls' if os.name == 'nt' else 'clear')
+
+ #ArgParse
+ parser = argparse.ArgumentParser(
+                     description = 'Modulo para coleta de informacoes em instancias EC2',
+                     prog ='Jarvis do Sagara',
+                     epilog = 'Contato: thiagosagara@gmail.com\n')
+
+ parser.add_argument('-f','--file', action='store',
+                     dest='gfile',default = 'notdefined',
+                     required = False,
+                     help = 'Digite o nome do arquivo onde estão os profiles')
+
+ parser.add_argument('-p','--profile', action='store',
+                     dest='gprofile',default = 'Sandbox',
+                     required = True,
+                     help = 'Digite o nome do profile (~/.aws/credentials)')
+                     
+ parser.add_argument('-d','--debug', action='store', type=int,
+                     dest='pshow', default = 0,
+                     required = False,
+                     help = 'adicione -d 1 para mostrar em modo debug')
+
+ #Instancia das opcoes
+
+ gfile=parser.parse_args().gfile
+ gprofile=parser.parse_args().gprofile
+ pshow=parser.parse_args().pshow
+ 
+ # --------------- Processo em caso de arquivo com os ICs ------------
+ # Valida se foi passado o argumento de arquivo
+ if gfile != "notdefined":
+  print("Feature em desenvolvimento")
+
+ else:
+  session = boto3.session.Session(profile_name = str(gprofile))
+   
+ #ROADMAP - Passar isso para uma funcao
+ print("account","region","instanceid","instancetype","hostname","state",sep=",")
+ client = session.client('ec2')
+ regions = client.describe_regions()
+ 
+ for i in regions['Regions']:
+  list_region(i['RegionName'], gprofile)
+  
