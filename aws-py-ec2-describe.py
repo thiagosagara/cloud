02ee3b05db7c3577b1b27fa7e5876e58c6a7e142
Thiago Sagara <thiagosagara@gmail.com>
2022-02-21 22:40:09 -0300

    First

---
+++ aws-py-ec2-describe.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#programa para listar o tipo de intancias em uma conta
+
+import boto3
+from botocore.config import Config
+import os
+
+
+profile = str(input("Digite o profile: "))
+os.system('cls' if os.name == 'nt' else 'clear')
+
+session = boto3.session.Session(profile_name = profile)
+client = session.client('ec2')
+
+regions = client.describe_regions()
+print("account","region","instanceid","instancetype","hostname",sep=",")
+
+def list_region(region, get_profile):
+ mc=Config( region_name = region )
+ conn=boto3.client('ec2', config=mc)
+ 
+ response = conn.describe_instances()
+ 
+ for r in response['Reservations']:
+  for i in r['Instances']:
+   if "KeyName" in i:
+    print(f"{profile},{region},{i['InstanceId']},{i['InstanceType']},{i['KeyName']}")
+   else:
+    print(f"{profile},{region},{i['InstanceId']},{i['InstanceType']},Sem Hostname")
+
+for i in regions['Regions']:
+ list_region(i['RegionName'], profile)
